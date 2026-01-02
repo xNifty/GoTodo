62376f442a668a7c7191c142e446856adb9d032e
xNifty <ryan@ryanmalacina.com>
2026-01-02 13:42:44 -0500

    Remove old CLI code; close #13

--- internal/tasks/list.go
+++ internal/tasks/list.go
@@ -4,9 +4,6 @@ import (
 	"GoTodo/internal/storage"
 	"context"
 	"fmt"
-	"os"
-	"strings"
-	"text/tabwriter"
 )
 
 const (
@@ -14,51 +11,6 @@ const (
 	GREEN = "\033[32m"
 	RESET = "\033[0m"
 )
-
-func ListTasks() {
-	pool, _ := storage.OpenDatabase()
-	defer storage.CloseDatabase(pool)
-
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
-
-	rows, err := pool.Query(context.Background(), "SELECT id, title, description, completed, time_stamp FROM tasks ORDER BY id")
-	if err != nil {
-		fmt.Println("Error in ListTasks (query):", err)
-	}
-	defer rows.Close()
-
-	headers := []string{"ID", "Title", "Description", "Status"}
-
-	fmt.Fprintln(writer, "\n"+strings.Join(headers, "\t"))
-
-	underlines := make([]string, len(headers))
-	for i, header := range headers {
-		underlines[i] = strings.Repeat("-", len(header))
-	}
-
-	fmt.Fprintf(writer, "%s", strings.Join(underlines, "\t")+"\n")
-
-	for rows.Next() {
-		var id int
-		var title string
-		var description string
-		var completed bool
-
-		err := rows.Scan(&id, &title, &description, &completed)
-
-		if err != nil {
-			fmt.Println("Error in ListTasks (scan):", err)
-		}
-
-		status := RED + "Incomplete" + RESET
-		if completed {
-			status = GREEN + "Complete" + RESET
-		}
-		fmt.Fprintf(writer, "%d\t%s\t%s\t%s\n", id, title, description, status)
-	}
-	writer.Flush()
-	fmt.Println()
-}
 
 func ReturnTaskList() []Task {
 	pool, _ := storage.OpenDatabase()
